3f8df6dda0641c20be79f1497c60bbdfb2993455
1f10230305 <s1f102303057@iniad.org>
2023-06-11 14:03:55 +0900

    remove: turn api

--- server/api/turn/controller.ts
+++
@@ -1,6 +0,0 @@
-import { boardRepository } from '$/repository/boardRepository';
-import { defineController } from './$relay';
-
-export default defineController(() => ({
-  get: () => ({ status: 200, body: boardRepository.turnColor() }),
-}));
